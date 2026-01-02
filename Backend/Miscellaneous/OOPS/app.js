3b6c6d0f38acc3e4c8dd4725413811d58c24486c
Nur Hasin Ahammad <nur.hasin.ahammad@gmail.com>
2026-01-02 23:21:00 +0600

    Add files via upload

--- Backend/Miscellaneous/OOPS/app.js
+++ Backend/Miscellaneous/OOPS/app.js
@@ -27,3 +27,30 @@ console.log("abc".toUpperCase === "xyz".toUpperCase);
 
 
 // Factory Functions
+function PersonMaker(name, age) {
+    const person = {
+        name: name,
+        age: age,
+        talk() {
+            console.log(`Hi, my name is ${this.name}`);
+        }
+    };
+
+    return person;
+}
+
+let p1 = PersonMaker("adam", 25);
+let p2 = PersonMaker("eve", 23);
+
+console.log(p1);
+console.log(p2);
+
+console.log(p1.talk());
+console.log(p2.talk());
+
+console.log(p1.talk === p2.talk);
+
+
+// Constructor
+
+
