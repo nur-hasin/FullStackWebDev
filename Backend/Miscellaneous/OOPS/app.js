a130aaaddbb6a3bbf65e111d275a99d49a1ca354
Nur Hasin Ahammad <nur.hasin.ahammad@gmail.com>
2026-01-05 06:58:06 +0600

    Add files via upload

--- Backend/Miscellaneous/OOPS/app.js
+++ Backend/Miscellaneous/OOPS/app.js
@@ -51,6 +51,129 @@ console.log(p2.talk());
 console.log(p1.talk === p2.talk);
 
 
-// Constructor
+// Constructors
+function Person(name, age) {
+    this.name = name;
+    this.age = age;
+    console.log(this);
+}
+
+Person.prototype.talk = function() {
+    console.log(`Hi, my name is ${this.name}`);
+}
+
+let p3 = new Person("adam", 25);
+let p4 = new Person("eve", 23);
+
+console.log(p3.talk === p4.talk);
 
 
+// Classes
+class Person2{
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+2
+    talk() {
+        console.log(`Hi, my name is ${this.name}`);
+    }
+}
+
+let p5 = new Person2("adam", 25);
+let p6 = new Person2("eve", 23);
+
+console.log(p5);
+console.log(p6);
+
+console.log(p5.talk === p6.talk);
+
+
+// Inheritance
+class Person3{
+    constructor(name, age) {
+        console.log("person class constructor");
+        this.name = name;
+        this.age = age;
+    }
+    talk() {
+        console.log(`Hi, my name is ${this.name}`);
+    }
+}
+
+class Student extends Person3 {
+    constructor(name, age, marks) {
+        console.log("student class constructor");
+        super(name, age);
+        this.marks = marks;
+    }
+}
+
+class Teacher extends Person3 {
+    constructor(name, age, subject) {
+        console.log("teacher class constructor");
+        super(name, age);
+        this.subject = subject;
+    }
+}
+
+let s1 = new Student("bob", 22, 97);
+
+console.log(s1);
+console.log(s1.name);
+console.log(s1.age);
+console.log(s1.marks);
+console.log(s1.talk());
+
+let t1 = new Teacher("Eve", 43, "Science");
+
+console.log(t1);
+console.log(t1.name);
+console.log(t1.age);
+console.log(t1.subject);
+console.log(t1.talk());
+
+
+class Mammal {
+    constructor(name) {
+        this.name = name;
+        this.type = "warm-blooded";
+    }
+    eat() {
+        console.log("I am eating");
+    }
+}
+
+class Dog extends Mammal {
+    constructor(name) {
+        super(name);
+    }
+    bark() {
+        console.log("ghauuu..");
+    }
+    eat() {
+        console.log("dog is eating");
+    }
+}
+
+class Cat extends Mammal {
+    constructor(name) {
+        super(name);
+    }
+    meow() {
+        console.log("meowww..");
+    }
+}
+
+let d1 = new Dog("juby");
+
+console.log(d1);
+console.log(d1.name);
+console.log(d1.type);
+console.log(d1.bark());
+console.log(d1.eat());
+
+let c1 = new Cat("Lucy");
+
+console.log(c1);
+console.log(c1.eat());
