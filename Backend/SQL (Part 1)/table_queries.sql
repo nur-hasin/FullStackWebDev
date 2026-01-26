ac93779e099f238c0cf0cffa5b459d8b51be71f8
Nur Hasin Ahammad <nur.hasin.ahammad@gmail.com>
2026-01-26 22:56:28 +0600

    Add files via upload

--- Backend/SQL (Part 1)/table_queries.sql
+++ Backend/SQL (Part 1)/table_queries.sql
@@ -40,3 +40,56 @@ VALUES
 (103, "Hello Delta", 3);
 
 SELECT * FROM post;
+
+
+INSERT INTO user 
+(id, age, name, email, followers, following)  
+VALUES
+(5, 14, "eve", "eve@yahoo.in", 400, 145),
+(6, 16, "jack", "jack@gmail.com", 10000, 1000);
+
+
+SET SQL_SAFE_UPDATES = 0;
+
+UPDATE user
+SET followers = 600
+WHERE age = 16;
+
+
+DROP TABLE post;
+
+DELETE FROM user
+WHERE age = 14;
+
+
+ALTER TABLE user
+ADD COLUMN city VARCHAR(20) DEFAULT "Dhaka";
+
+ALTER TABLE user
+DROP COLUMN age;
+
+ALTER TABLE user
+RENAME TO insta_user;
+
+SELECT * FROM insta_user;
+
+ALTER TABLE insta_user
+RENAME TO user;
+
+ALTER TABLE user
+CHANGE COLUMN followers subs INT DEFAULT 0;
+
+ALTER TABLE user
+MODIFY subs INT DEFAULT 10;
+
+INSERT INTO user
+(id, name, email, following)  
+VALUES
+(7, "rock", "rock@yahoo.in", 120);
+
+
+TRUNCATE TABLE user;
+
+
+
+SELECT * FROM user;
