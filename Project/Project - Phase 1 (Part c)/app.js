647d7351a768c81f7b96503207f8d82421853512
Nur Hasin Ahammad <nur.hasin.ahammad@gmail.com>
2026-02-18 23:47:48 +0600

    Add files via upload

--- Project/Project - Phase 1 (Part c)/app.js
+++ Project/Project - Phase 1 (Part c)/app.js
@@ -9,6 +9,7 @@ const methodOverride = require('method-override');
 const ejsMate = require('ejs-mate');
 const wrapAsync = require('./utils/wrapAsync.js');
 const ExpressError = require('./utils/ExpressError.js');
+const { listingSchema } = require('./schema.js');
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -27,6 +28,16 @@ main()
 
 async function main() {
     await mongoose.connect(MONGO_URL);
+};
+
+const validateListing = (req, res, next) => {
+    let { error } = listingSchema.validate(req.body);
+    if (error) {
+        let errMsg = error.details.map((el) => el.message).join(',');
+        throw new ExpressError(400, errMsg);
+    } else {
+        next();
+    }
 };
 
 //root route
@@ -53,7 +64,7 @@ app.get('/listings/:id', wrapAsync(async (req, res) => {
 }));
 
 //create route
-app.post('/listings', wrapAsync(async (req, res, next) => {
+app.post('/listings', validateListing, wrapAsync(async (req, res, next) => {
     const newListing = new Listing(req.body.listing);
     await newListing.save();
     res.redirect('/listings');
@@ -67,7 +78,7 @@ app.get('/listings/:id/edit', wrapAsync(async (req, res) => {
 }));
 
 //update route
-app.put('/listings/:id', wrapAsync(async (req, res) => {
+app.put('/listings/:id', validateListing, wrapAsync(async (req, res) => {
     let { id } = req.params;
     await Listing.findByIdAndUpdate(
         id,
@@ -88,8 +99,8 @@ app.use((req, res, next) => {
     next(new ExpressError(404, "Page Not Found!"));
 });
 
-//error middleware
 app.use((err, req, res, next) => {
+    console.log(err);
     let { statusCode = 500, message = 'someting went wrong!' } = err;
-    res.status(statusCode).send(message);
+    res.status(statusCode).render('error.ejs', { message });
 });
