68c5f83c87a299ade6b895bed641e644ff68804c
Nur Hasin Ahammad <nur.hasin.ahammad@gmail.com>
2026-03-10 11:32:26 +0600

    Add files via upload

--- Backend/Backend 10 (Cookies)/server.js
+++ Backend/Backend 10 (Cookies)/server.js
@@ -2,6 +2,30 @@ const express = require('express');
 const app = express();
 const users = require('./routes/users.js');
 const posts = require('./routes/posts.js');
+const cookieParser = require('cookie-parser');
+
+app.use(cookieParser('secretcode'));
+
+app.get('/getSignedCookie', (req, res) => {
+    res.cookie('madeIn', 'Bangladesh', { signed: true });
+    res.send('signed cookie sent');
+});
+
+app.get('/verify', (req, res) => {
+    console.log(req.signedCookies);
+    res.send('verified');
+});
+
+app.get('/greet', (req, res) => {
+    let { name = 'anonymous' } = req.cookies;
+    res.send(`Hi, ${name}`);
+});
+
+app.get('/getCookies', (req, res) => {
+    res.cookie('greet', 'hello');
+    res.cookie('madeIn', 'Bangladesh');
+    res.send('sent you some cookies!');
+});
 
 app.use('/users', users);
 app.use('/posts', posts);
