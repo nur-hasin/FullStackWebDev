153d47c378eb4438c6cd8d665cb8491b89ecc99d
Nur Hasin Ahammad <nur.hasin.ahammad@gmail.com>
2026-03-11 23:27:49 +0600

    Add files via upload

--- Backend/Backend 11 (Sessions)/server.js
+++ Backend/Backend 11 (Sessions)/server.js
@@ -1,21 +1,39 @@
 const express = require('express');
 const app = express();
 const session = require('express-session');
+const flash = require("connect-flash");
+const path = require('path');
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 app.use(session({
     secret: 'mysupersecretstring',
     resave: false,
     saveUninitialized: true
 }));
+app.use(flash());
+
+app.use((req, res, next) => {
+    res.locals.successMsg = req.flash('success');
+    res.locals.errorMsg = req.flash('error');
+    next();
+});
 
 app.get('/register', (req, res) => {
     let { name = 'anonymous' } = req.query;
     req.session.name = name;
+
+    if (name === 'anonymous') {
+        req.flash('error', 'user not registered!');
+    } else {
+        req.flash('success', 'user registered successfully!');
+    }
+
     res.redirect('/hello');
 });
 
 app.get('/hello', (req, res) => {
-    res.send(`hello, ${req.session.name}`);
+    res.render('page', { name: req.session.name });
 });
 
 app.get('/req-count', (req, res) => {
